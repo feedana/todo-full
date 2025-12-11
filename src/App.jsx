510f2671807833f158ac4aa1685ff9c677ed6b1a
feedana <fdn.shirinli@gmail.com>
2025-12-11 15:42:33 +0100

    upp

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import Todos from "./Components/Todo";
 
 export default function App() {
   return (
-    <div className="p-10 bg-gray-100 min-h-screen">
-      <h1 className="text-3xl font-bold mb-6">FS-42 TODO APP!</h1>
+  <div className="p-10 bg-blue-100 min-h-screen">
+  <h1 className="text-3xl font-bold mb-6 text-center text-blue-900"> ToDo APP</h1>
+
       <Todos />
     </div>
   );
